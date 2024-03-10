70987727e89376f6098566eb3fdccc2ab708de5f
darshan45672 <drshnbhandary@gmail.com>
2024-03-10 17:33:16 +0530

    added nav

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -10,8 +10,8 @@
 
         <nav id="navbar" class="navbar">
             <ul>
-                <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-                <li><a class="nav-link scrollto" href="#about">About</a></li>
+                <li><a class="nav-link scrollto active" href="{{ route('home') }}">Home</a></li>
+                <li><a class="nav-link scrollto" href="{{ route('about-us') }}">About</a></li>
                 <li><a class="nav-link scrollto" href="#services">Services</a></li>
                 <li><a class="nav-link scrollto" href="#portfolio">Portfolio</a></li>
                 <li><a class="nav-link scrollto" href="#team">Team</a></li>
