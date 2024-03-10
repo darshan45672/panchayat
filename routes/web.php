546d74866696b212b7d6b2b974714aa632556403
darshan45672 <drshnbhandary@gmail.com>
2024-03-10 16:19:37 +0530

    added frontent home template

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Livewire\ShowHome;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', ShowHome::class)->name('home');
