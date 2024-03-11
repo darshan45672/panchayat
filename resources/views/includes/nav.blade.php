fde17d1298ee93d6ccaeabc82d93916775d480ad
darshan45672 <drshnbhandary@gmail.com>
2024-03-11 07:31:21 +0530

    added route

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -4,8 +4,6 @@
 
         <div class="logo">
         <h1><a href="{{ route('home') }}"><span>e</span>Business</a></h1>
-            <!-- Uncomment below if you prefer to use an image logo -->
-            <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
         </div>
 
         <nav id="navbar" class="navbar">
