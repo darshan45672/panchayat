da88a4f6a7940acc7cc5d4772cee879c94eb83c0
darshan45672 <drshnbhandary@gmail.com>
2024-03-11 07:32:15 +0530

    added route

--- routes/web.php
+++ routes/web.php
@@ -23,5 +23,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', ShowHome::class)->name('home');
 Route::get('/about', ShowAboutUs::class)->name('about-us');
-Route::get('/service', ShowServices::class)->name('services');
+Route::get('/services', ShowServices::class)->name('services');
 Route::get('/contact', ContactUs::class)->name('contact');
